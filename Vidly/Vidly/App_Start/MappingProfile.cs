206c17f4265bd55fa04466f60034a6801bf5faec
[AlexKami0115] <[kami0115@bears.unco.edu]>
2020-07-21 17:09:28 -0500

    adding packages, config files

---
+++ Vidly/Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+            Mapper.CreateMap<Genre, GenreDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+        }
+    }
+}
